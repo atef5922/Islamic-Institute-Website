f34f003aa02ceeb8aa0b35f30216fc04e21002de
atef5922 <sifat15-5922@gmail.com>
2026-06-22 13:13:41 +0600

    replace navbar banner and fix the bugs

--- src/components/layout/MadrasaIdentityBanner.tsx
+++ src/components/layout/MadrasaIdentityBanner.tsx
@@ -1,24 +1,16 @@
 /* eslint-disable @next/next/no-img-element */
-import desktopBanner from "@/product/navbar-banner-desktop.png";
-import laptopBanner from "@/product/navbar-banner-laptop.png";
-import mobileBanner from "@/product/navbar-banner-mobile.png";
-import tabletBanner from "@/product/navbar-banner-tablet.png";
+import finalBanner from "@/product/navbar-final-banner.webp";
 
 export function MadrasaIdentityBanner() {
   return (
     <section className="block w-full overflow-hidden border-y border-gold/25 bg-[#fff7df]">
-      <picture className="block w-full">
-        <source media="(min-width: 1200px)" srcSet={desktopBanner.src} />
-        <source media="(min-width: 1024px)" srcSet={laptopBanner.src} />
-        <source media="(min-width: 640px)" srcSet={tabletBanner.src} />
-        <img
-          src={mobileBanner.src}
-          alt="মাদরাসাতুল হিদায়াহ নেভবার ব্যানার"
-          className="block h-auto w-full object-center"
-          decoding="async"
-          fetchPriority="high"
-        />
-      </picture>
+      <img
+        src={finalBanner.src}
+        alt="Madrasa banner"
+        className="block h-auto w-full object-contain"
+        decoding="async"
+        fetchPriority="high"
+      />
     </section>
   );
 }
